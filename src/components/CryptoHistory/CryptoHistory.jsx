5de05cf720d4d9bea9b8eb92740d44a0fb9a76a5
USER220299 <127625631+USER220299@users.noreply.github.com>
2023-09-24 20:47:40 +0300

    task-4

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,28 @@
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+import {getData}  from '../Service/DataTransform'
+export const CryptoHistory = ({items}) => {
+  return (<BaseTable>
+  <THead>
+    <tr>
+      <Th>№</Th>
+      <Th>PRICE</Th>
+      <Th>AMOUNT</Th>
+      <Th>DATE</Th>
+    </tr>
+  </THead>
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+    <tbody>
+      {items.map(({ id, price, amount, date,  }, index) => (   <Tr key={id}>
+        <Td>{index+1}</Td>
+        <Td>{price}</Td>
+        <Td>{amount}</Td>
+        <Td>{getData(date)}</Td>
+    </Tr>)
+       
+        
+      )}
+  
+   
+  </tbody>
+</BaseTable>)
 };
--- src/components/Service/DataTransform.jsx
+++ src/components/Service/DataTransform.jsx
@@ -1,5 +1,10 @@
-import { formatDistanceToNow } from 'date-fns';
+import { format, formatDistanceToNow } from 'date-fns';
+
 
 export function dataTransform(data) {
   return formatDistanceToNow(new Date(data), { addSuffix: true });
+}
+
+export function getData(data) {
+ return  format(new Date(data), "Pp")
 }
